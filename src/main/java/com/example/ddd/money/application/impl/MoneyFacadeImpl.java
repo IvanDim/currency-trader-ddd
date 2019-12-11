824066feb0697d45ea6ce93ab72db2b24fc2906f
Ivan Dimitrov <darksage@abv.bg>
2019-12-11 12:05:18 +0100

    Added authentication Service calling an security context service to auth the user

--- src/main/java/com/example/ddd/money/application/impl/MoneyFacadeImpl.java
+++ src/main/java/com/example/ddd/money/application/impl/MoneyFacadeImpl.java
@@ -2,6 +2,7 @@ package com.example.ddd.money.application.impl;
 
 import com.example.ddd.money.application.MoneyFacade;
 import com.example.ddd.money.domain.exception.UserNotFoundException;
+import com.example.ddd.money.domain.service.UserAuthenticationService;
 import com.example.ddd.user.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,18 @@ import java.util.Map;
 public class MoneyFacadeImpl implements MoneyFacade {
 
     private final UserRepository userRepository;
+    private final UserAuthenticationService userAuthenticationService;
 
     @Autowired
-    public MoneyFacadeImpl(UserRepository userRepository) {
+    public MoneyFacadeImpl(UserRepository userRepository, UserAuthenticationService userAuthenticationService) {
         this.userRepository = userRepository;
+        this.userAuthenticationService = userAuthenticationService;
     }
 
     @Override
     public Map<String, Double> userBalance(String token) {
-        return userRepository.findByUsername(token)
+        final var username = userAuthenticationService.validateTokenAndGetUsername(token);
+        return userRepository.findByUsername(username)
                 .orElseThrow(UserNotFoundException::new)
                 .getBalance();
     }
--- src/main/java/com/example/ddd/money/infrastructure/MoneyController.java
+++ src/main/java/com/example/ddd/money/infrastructure/MoneyController.java
@@ -3,12 +3,17 @@ package com.example.ddd.money.infrastructure;
 import com.example.ddd.money.application.MoneyFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+
+//TODO implement jax-rs in the future
 
 @RestController
 @RequestMapping("/money")
@@ -21,6 +26,7 @@ public class MoneyController {
         this.moneyFacade = moneyFacade;
     }
 
+    //TODO return an response object
     @GetMapping("/balance")
     public Map<String, Double> balance(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
         return moneyFacade.userBalance(token);
